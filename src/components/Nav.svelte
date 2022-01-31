d3fe168d2d8d85fd55e8429aef548e31bf6c8347
OJ <owen.jeffs@me.com>
2022-01-31 10:05:06 +0000

    initial commit, blank Sapper/Svelte Covid tacker app

---
+++ src/components/Nav.svelte
@@ -0,0 +1,13 @@
+<script>
+	// export let segment;
+</script>
+
+<nav>nav bar</nav>
+
+<style>
+	nav {
+		border-bottom: 1px solid rgba(255, 62, 0, 0.1);
+		font-weight: 300;
+		padding: 0 1em;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script>
+	import successkid from 'images/successkid.jpg';
+</script>
+
+<svelte:head>
+	<title>Covid Tracker</title>
+</svelte:head>
+
+<style>
+</style>
